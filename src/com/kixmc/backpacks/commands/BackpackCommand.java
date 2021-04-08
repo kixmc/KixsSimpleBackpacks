d2aede39ad4fbd371510d353fb3e11b829ef0187
kixmc <45376097+kixmc@users.noreply.github.com>
2021-04-08 17:02:41 -0400

    Add reload command & permissions

--- src/com/kixmc/backpacks/commands/BackpackCommand.java
+++ src/com/kixmc/backpacks/commands/BackpackCommand.java
@@ -1,39 +1,68 @@
 package com.kixmc.backpacks.commands;
 
 import com.kixmc.backpacks.core.SimpleBackpacks;
-import com.kixmc.backpacks.contents.ItemHandler;
 import com.kixmc.backpacks.utils.BackpackItem;
-import com.kixmc.backpacks.utils.ChatUtil;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
-
-import java.util.ArrayList;
 
 public class BackpackCommand implements CommandExecutor {
 
-    @Override
+    public final String noPermission = ChatColor.RED + "You don't have permission to do that.";
+    public final String usage = ChatColor.RED + "/backpacks <get|reload>";
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player) {
+        if (args.length == 0 || args.length > 1) {
 
-            Player p = (Player) sender;
+            sender.sendMessage(usage);
 
-            if (args.length > 0) {
-                if (args[0].equalsIgnoreCase("get")) {
+            return true;
+        }
+
+        if (args.length == 1) {
+
+            switch (args[0].toLowerCase()) {
+
+                case "get":
+                    if (!(sender instanceof Player)) {
+                        sender.sendMessage(ChatColor.RED + "You have to be a player to obtain a backpack!");
+                        return true;
+                    }
+
+                    Player p = (Player) sender;
+
+                    if (!p.hasPermission("backpacks.getcommand")) {
+                        p.sendMessage(noPermission);
+                        return true;
+                    }
 
                     p.getInventory().addItem(BackpackItem.makeUnopened());
 
-                }
+                    break;
+
+                case "reload":
+                    if (!sender.hasPermission("backpacks.reload")) {
+                        sender.sendMessage(noPermission);
+                        return true;
+                    }
+
+                    SimpleBackpacks.get().reloadConfig();
+
+                    sender.sendMessage(ChatColor.GREEN + "KixsSimpleBackpacks has been reloaded!");
+
+                    break;
+
+                default:
+                    sender.sendMessage(usage);
+                    break;
+
             }
 
         }
+
         return false;
     }
 
