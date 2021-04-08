28da9bbe865b1f9ff6c79e8612aea8d51cd0088e
kixmc <45376097+kixmc@users.noreply.github.com>
2021-04-08 01:41:35 -0400

    Add backback renaming support Fix NPE related to InventoryClickEvent

--- src/com/kixmc/backpacks/listeners/InventoryClick.java
+++ src/com/kixmc/backpacks/listeners/InventoryClick.java
@@ -1,15 +1,33 @@
 package com.kixmc.backpacks.listeners;
 
 import com.kixmc.backpacks.utils.BackpackUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class InventoryClick implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent e) {
-        if(BackpackUtils.isBackpack(e.getCurrentItem()) && e.getView().getTitle().equals("Backpack")) e.setCancelled(true);
+
+        if (BackpackUtils.isBackpack(e.getCurrentItem()) && e.getView().getTitle().equals("Backpack")) e.setCancelled(true);
+
+        if (!(e.getWhoClicked() instanceof Player)) return;
+        if (e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR || e.getInventory().getType() != InventoryType.ANVIL) return;
+
+        if (e.getSlotType() == InventoryType.SlotType.RESULT) {
+            if (!BackpackUtils.isBackpack(e.getCurrentItem())) return;
+            ItemMeta im = e.getCurrentItem().getItemMeta();
+            im.setDisplayName(ChatColor.YELLOW + "Backpack: '" + ChatColor.RESET + e.getCurrentItem().getItemMeta().getDisplayName() + ChatColor.YELLOW + "'");
+            e.getCurrentItem().setItemMeta(im);
+        }
+
     }
 
 }
--- src/com/kixmc/backpacks/utils/BackpackUtils.java
+++ src/com/kixmc/backpacks/utils/BackpackUtils.java
@@ -1,6 +1,7 @@
 package com.kixmc.backpacks.utils;
 
 import com.kixmc.backpacks.core.SimpleBackpacks;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -12,6 +13,7 @@ public class BackpackUtils {
     public static boolean isBackpack(ItemStack is) {
 
         if(is == null) return false;
+        if(is.getType() == Material.AIR) return false;
 
         NamespacedKey key = new NamespacedKey(SimpleBackpacks.get(), "kixs-backpacks");
         ItemMeta itemMeta = is.getItemMeta();
