3cad41f7f20d60425cc830307c3ffb15d5f000b8
kixmc <45376097+kixmc@users.noreply.github.com>
2021-04-09 11:20:02 -0400

    Change EventPriority to LOWEST & more code cleanup

--- src/com/kixmc/backpacks/listeners/InventoryClose.java
+++ src/com/kixmc/backpacks/listeners/InventoryClose.java
@@ -5,6 +5,7 @@ import com.kixmc.backpacks.core.SimpleBackpacks;
 import com.kixmc.backpacks.utils.BackpackUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
@@ -16,23 +17,22 @@ import java.util.Objects;
 
 public class InventoryClose implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onInventoryClose(InventoryCloseEvent e) {
 
         if (e.getPlayer().getInventory().getItem(e.getPlayer().getInventory().getHeldItemSlot()) == null) return;
 
-        if (BackpackUtils.isBackpack(e.getPlayer().getInventory().getItem(e.getPlayer().getInventory().getHeldItemSlot())) && e.getView().getTitle().equals(SimpleBackpacks.get().getConfig().getString("backpack.gui-title"))) {
+        if (!(BackpackUtils.isBackpack(e.getPlayer().getInventory().getItem(e.getPlayer().getInventory().getHeldItemSlot())) && e.getView().getTitle().equals(SimpleBackpacks.get().getConfig().getString("backpack.gui-title")))) return;
 
-            Inventory dummyInventory = Bukkit.createInventory(e.getPlayer(), 54, "");
-            Arrays.stream(e.getInventory().getContents()).filter(Objects::nonNull).forEach(dummyInventory::addItem);
+        Inventory dummyInventory = Bukkit.createInventory(e.getPlayer(), 54, "");
+        Arrays.stream(e.getInventory().getContents()).filter(Objects::nonNull).forEach(dummyInventory::addItem);
 
-            ArrayList<ItemStack> tidiedContents = new ArrayList<>();
+        ArrayList<ItemStack> tidiedContents = new ArrayList<>();
 
-            Arrays.stream(dummyInventory.getContents()).filter(Objects::nonNull).forEach(tidiedContents::add);
+        Arrays.stream(dummyInventory.getContents()).filter(Objects::nonNull).forEach(tidiedContents::add);
 
-            ItemHandler.store(e.getPlayer().getInventory().getItem(e.getPlayer().getInventory().getHeldItemSlot()), tidiedContents);
+        ItemHandler.store(e.getPlayer().getInventory().getItem(e.getPlayer().getInventory().getHeldItemSlot()), tidiedContents);
 
-        }
     }
 
 }
